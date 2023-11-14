69df00345b38eb365c77ff33dcc76d16dbfe656c
yeonseochae <chaey826@newschool.edu>
2023-11-14 02:41:11 -0500

    entry orange with javascript

---
+++ harmonic-collection/Homepage/orange/script.js
@@ -0,0 +1,9 @@
+ 
+const myBtn = document.getElementById("myBtn");
+myBtn. addEventListener("click", function(e){
+    const name= prompt ("What is 주황 in English?");
+    document.body.innerHTML = "<p> Orange is correct! <br> Orange is the color of enthusiasm and emotion. Orange exudes warmth <br> and joy  and is considered a fun color that provides emotional strength. <br> It is optimistic and uplifting, adds spontaneity and positivity to <br> life and it encourages social communication and creativity. <br> It is a youthful and energetic color.</p>"
+    ;
+});
+
+
