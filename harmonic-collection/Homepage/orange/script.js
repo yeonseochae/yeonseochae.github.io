6954bb58cdf61c70737e59be7b32d726834285cf
yeonseochae <chaey826@newschool.edu>
2023-12-11 23:33:18 -0500

    orange cursor update

--- harmonic-collection/Homepage/orange/script.js
+++ harmonic-collection/Homepage/orange/script.js
@@ -5,5 +5,24 @@ myBtn. addEventListener("click", function(e){
     document.body.innerHTML = "<p> Orange is correct! <br> Orange is the color of enthusiasm and emotion. Orange exudes warmth <br> and joy  and is considered a fun color that provides emotional strength. <br> It is optimistic and uplifting, adds spontaneity and positivity to <br> life and it encourages social communication and creativity. <br> It is a youthful and energetic color.</p>"
     ;
 });
+// customized cursor (chat gpt)
+document.addEventListener("DOMContentLoaded", function () {
+    const customCursor = document.getElementById("custom-cursor");
 
+    document.addEventListener("mousemove", function (e) {
+      // Update the position of the custom cursor
+      customCursor.style.left = e.pageX - customCursor.offsetWidth / 2 + "px";
+      customCursor.style.top = e.pageY - customCursor.offsetHeight / 2 + "px";
+    });
+
+    document.addEventListener("mouseenter", function () {
+      // Show the custom cursor when inside the document
+      customCursor.style.display = "block";
+    });
+
+    document.addEventListener("mouseleave", function () {
+      // Hide the custom cursor when outside the document
+      customCursor.style.display = "none";
+    });
+  });
 
